0ea82f03c47cec71644d51b7f77a2a50fe5e58ae
Kevin Mallinson <kevin.mallinson@outlook.com>
2020-10-12 14:41:09 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 npm run build
 git subtree split --branch gh-pages --prefix dist/
-git push git@github.com:kevinmallinson/portfolio.git gh-pages:gh-pages
+git push -f git@github.com:kevinmallinson/portfolio.git gh-pages:gh-pages
 rm -rf dist/
