0aa329ea3f79305a905cd64e0fe1bc26f3c5d6cc
Kevin Mallinson <kevin.mallinson@outlook.com>
2020-10-11 21:33:31 -0400

    Refactor experience and add bootstrap vue

--- src/components/experience.vue
+++ src/components/experience.vue
@@ -1,198 +1,130 @@
 <template>
-	<section class="mytabs_area p_120">
-		<div class="container">
-			<div class="tabs_inner">
-				<div class="experience-header">
-					<h1>My Experience</h1>
-				</div>
-				<div class="tab-content" id="myTabContent">
-					<div
-						class="tab-pane fade show active"
-						id="home"
-						role="tabpanel"
-						aria-labelledby="home-tab"
-					>
-						<ul class="list">
-							<li>
-								<span></span>
-								<div class="media">
-									<div class="d-flex experience-date">
-										<p>Oct 2020 - Present</p>
-									</div>
-									<div class="media-body">
-										<h4>Flynn Group</h4>
-										<p>Web Application Developer <br />Toronto, Canada</p>
-									</div>
+	<b-container class="experience-container">
+		<b-row>
+			<b-col cols="12" class="experience-header p-5">
+				<h1>My Experience</h1>
+			</b-col>
+		</b-row>
+		<b-row>
+			<b-col cols="12">
+				<ul class="experience-list">
+						<li>
+							<span></span>
+							<div class="media">
+								<div class="d-flex experience-date">
+									<p>Oct 2020 - Present</p>
 								</div>
-							</li>
-							<li>
-								<span></span>
-								<div class="media">
-									<div class="d-flex  experience-date">
-										<p>Mar 2020 - Sep 2020</p>
-									</div>
-									<div class="media-body">
-										<h4>Inlogik</h4>
-										<p>Software Engineer <br />Melbourne, Australia</p>
-									</div>
+								<div class="media-body">
+									<h4>Flynn Group</h4>
+									<p>Web Application Developer <br />Toronto, Canada</p>
 								</div>
-							</li>
-							<li>
-								<span></span>
-								<div class="media">
-									<div class="d-flex  experience-date">
-										<p>Sep 2019 - Dec 2020</p>
-									</div>
-									<div class="media-body">
-										<h4>
-											Your HR System <br />
-											<small>3 Month Contract</small>
-										</h4>
-										<p>Full Stack Web Developer <br />Melbourne, Australia</p>
-									</div>
+							</div>
+						</li>
+						<li>
+							<span></span>
+							<div class="media">
+								<div class="d-flex experience-date">
+									<p>Mar 2020 - Sep 2020</p>
 								</div>
-							</li>
-							<li>
-								<span></span>
-								<div class="media">
-									<div class="d-flex  experience-date">
-										<p>Jul 2018 - Feb 2020</p>
-									</div>
-									<div class="media-body">
-										<h4>ANZUK Education</h4>
-										<p>Full Stack Web Developer <br />Melbourne, Australia</p>
-									</div>
+								<div class="media-body">
+									<h4>Inlogik</h4>
+									<p>Software Engineer <br />Melbourne, Australia</p>
 								</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+							</div>
+						</li>
+						<li>
+							<span></span>
+							<div class="media">
+								<div class="d-flex experience-date">
+									<p>Sep 2019 - Dec 2020</p>
+								</div>
+								<div class="media-body">
+									<h4>
+										Your HR System <br />
+										<small>3 Month Contract</small>
+									</h4>
+									<p>Full Stack Web Developer <br />Melbourne, Australia</p>
+								</div>
+							</div>
+						</li>
+						<li>
+							<span></span>
+							<div class="media">
+								<div class="d-flex experience-date">
+									<p>Jul 2018 - Feb 2020</p>
+								</div>
+								<div class="media-body">
+									<h4>ANZUK Education</h4>
+									<p>Full Stack Web Developer <br />Melbourne, Australia</p>
+								</div>
+							</div>
+						</li>
+					</ul>
+			</b-col>
+		</b-row>
+	</b-container>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss">
-	.experience-header h1 {
-		text-align: center;
+<style scoped>
+	.experience-container {
 		color: white;
-		font-family: "Heebo", sans-serif;
-		font-size: 36px;
-		padding-bottom: 70px;
-		text-transform: uppercase;
+	}
+
+	.experience-header {
+		text-align: center;
 	}
 
 	.experience-date {
 		min-width: 250px;
 	}
 
-	.mytabs_area{
-		background-image: -moz-linear-gradient( 0deg, rgb(118,109,255) 0%, rgb(136,243,255) 100%);
-		background-image: -webkit-linear-gradient( 0deg, rgb(118,109,255) 0%, rgb(136,243,255) 100%);
-		background-image: -ms-linear-gradient( 0deg, rgb(118,109,255) 0%, rgb(136,243,255) 100%);
+	.experience-list {
+		max-width: 460px;
+		margin: auto;
+		position: relative;
+		padding-top: 40px;
 	}
-	.tabs_inner{
-		.nav.nav-tabs{
-			display: block;
-			text-align: center;
-			border: none;
-			margin-bottom: 120px;
-			li{
-				display: inline-block;
-				margin-right: 8px;
-				a{
-					margin: 0px;
-					line-height: 50px;
-					border-radius: 5px;
-					padding: 0px 40px;
-					font-size: 13px;
-					font-weight: 500;
-					color: #fff;
-					font-family: $rob;
-					border: 1px solid #9ab5f5;
-					background: rgba(255, 255, 255, .1);
-					&.active{
-						background: #fff;
-						color: $dip;
-					}
-				}
-			}
-		}
-		.tab-content{
-			.tab-pane{
-				.list{
-					max-width: 460px;
-					margin: auto;
-					position: relative;
-					padding-top: 40px;
-					padding-bottom: 60px;
-					&:before{
-						content: "";
-						height: 100%;
-						width: 5px;
-						background: rgba(255, 255, 255, .20);
-						position: absolute;
-						left: 46%;
-						transform: translateX(-50%);
-						top: -5px;
-					}
-					li{
-						margin-bottom: 60px;
-						position: relative;
-						span{
-							height: 15px;
-							width: 15px;
-							border-radius: 50%;
-							display: block;
-							background: rgba(255, 255, 255, .2);
-							position: absolute;
-							left: 46%;
-							top: 30px;
-							transform: translateX(-50%);
-							&:before{
-								content: '';
-								height: 7px;
-								width: 7px;
-								background: #fff;
-								border-radius: 50%;
-								position: absolute;
-								left: 52%;
-								top: 4px;
-								transform: translateX(-50%);
-							}
-						}
-						&:last-child{
-							margin-bottom: 0px;
-						}
-						.media{
-							.d-flex{
-								padding-right: 100px;
-								p{
-									color: rgba(255, 255, 255, .75);
-									margin-bottom: 0px;
-									padding-top: 20px;
-								}
-							}
-							.media-body{
-								h4{
-									color: #fff;
-									font-size: 21px;
-									text-transform: uppercase;
-									margin-bottom: 20px;
-								}
-								p{
-									color: rgba(255, 255, 255, .75);
-									margin-bottom: 0px;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
+
+	.experience-list:before {
+		content: "";
+		height: 100%;
+		width: 5px;
+		background: rgba(255, 255, 255, 0.2);
+		position: absolute;
+		left: 46%;
+		transform: translateX(-50%);
+		top: -5px;
 	}
+
+	li {
+		margin-bottom: 60px;
+	}
+
+	li span {
+		height: 15px;
+		width: 15px;
+		border-radius: 50%;
+		display: block;
+		background: rgba(255, 255, 255, 0.2);
+		position: absolute;
+		left: 46%;
+		transform: translateX(-50%);
+	}
+
+	li span:before {
+		content: "";
+		height: 7px;
+		width: 7px;
+		background: #fff;
+		border-radius: 50%;
+		position: absolute;
+		left: 52%;
+		top: 4px;
+		transform: translateX(-50%);
+	}
+	
 </style>
