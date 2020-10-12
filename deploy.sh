68b6d6fea9478fe42bb2fb3a5e5d5e8dad880327
Kevin Mallinson <kevin.mallinson@outlook.com>
2020-10-12 14:51:34 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
 
+rm -rf dist
 npm run build
-git subtree split --branch gh-pages --prefix dist/
-git push -f git@github.com:kevinmallinson/portfolio.git gh-pages:gh-pages
-rm -rf dist/
+
+cd dist
+git init
+
+echo "kmallinson.ca" > CNAME
+
+git add .
+git commit -m "Deploy"
+
+git remote add origin git@github.com:kevinmallinson/portfolio.git
+git push --force origin master:gh-pages
