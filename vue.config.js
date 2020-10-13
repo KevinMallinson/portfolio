f91a99a01d0684a36e5317f918ab612e5ed8a3a8
Kevin Mallinson <kevin.mallinson@outlook.com>
2020-10-12 21:43:45 -0400

    Update Path

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,7 @@
 const path = require('path');
 
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production'
-		? '/portfolio/'
-		: '/',
+	publicPath: '/',
 	css: {
 		loaderOptions: {
 			sass: {
